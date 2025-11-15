df14b2464ba34b1b476b6cdc935099521a74e668
Nahruz01 <wanhairulazmy@gmail.com>
2025-11-15 23:05:00 +0800

    Setup basic React routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
-import { Component } from "react";
-import Navbar from "./_component/NavBar";
+// src/App.jsx
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./_component/NavBar.jsx";
+import Home from "./_pages/Home.jsx";
 import About from "./_pages/About.jsx";
 import Explore from "./_pages/Explore.jsx";
 import PantunPen from "./_pages/PantunPen.jsx";
 
 export default function App() {
-  let Page;
-  switch (window.location.pathname) {
-    case "/PantunPen":
-      Page = PantunPen;
-      break;
-    case "/Explore":
-      Page = Explore;
-      break;
-    case "/About":
-      Page = About;
-      break;
-    default:
-      Page = About;
-      break;
-  }
-
   return (
     <>
-    <Navbar />
-    <Page />
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Explore" element={<Explore />} />
+        <Route path="/PantunPen" element={<PantunPen />} />
+      </Routes>
     </>
   );
 }
--- src/_component/NavBar.jsx
+++ src/_component/NavBar.jsx
@@ -1,27 +1,27 @@
 // src/_component/NavBar.jsx
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="nav">
-        <a href="/" className="i-pantun-title"> 
-            I-Pantun 
-        </a>
-        <ul>
-            <CustomLink href="/">Home</CustomLink>
-            <CustomLink href="/Explore">Explore</CustomLink>
-            <CustomLink href="/PantunPen">PantunPen</CustomLink>    
-            <CustomLink href="/About">About</CustomLink>
-        </ul>
+      <Link to="/" className="i-pantun-title">
+        I-Pantun
+      </Link>
+      <ul>
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/Explore">Explore</CustomLink>
+        <CustomLink to="/PantunPen">PantunPen</CustomLink>
+        <CustomLink to="/About">About</CustomLink>
+      </ul>
     </nav>
   );
 }
 
-function CustomLink({ href, children }) {
-    const pathname = window.location.pathname;
-
-    return (
-        <li className={pathname === href ? "active" : ""}>
-            <a href={href}>{children}</a>
-        </li>
-    );
+function CustomLink({ to, children }) {
+  const location = useLocation();
+  return (
+    <li className={location.pathname === to ? "active" : ""}>
+      <Link to={to}>{children}</Link>
+    </li>
+  );
 }
